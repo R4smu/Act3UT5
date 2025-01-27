13f6713d31774725ea0733dad61dfc055a6b9653
R4smu <132304286+R4smu@users.noreply.github.com>
2025-01-27 17:25:01 +0100

    Delete javadoc.java

--- javadoc.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random();
-import java.util.Arrays();
-public class pruebaprueba{
-    public static void main(String[] args) throws Exception { 
-   
-    int [] numero = new int [5];
-    
-    }}
